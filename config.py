39229a3c922410da3f31f8780496be7afa60dbda
𝐉𝐀𝐒𝐒 <150457649+jaskrnsroay@users.noreply.github.com>
2024-07-30 16:24:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ EXTRA_PLUGINS_FOLDER = getenv("EXTRA_PLUGINS_FOLDER", "plugins")
 
 
 LOGGER_ID = int(getenv("LOGGER_ID"))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001443337704"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002050705178"))
 
 # Get this value from  on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID"))
@@ -62,8 +62,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/VIP_CREATORS")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TG_FRIENDSS")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/dugguworld")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Dilwale_Haven")
 
 # Maximum Limit Allowed for users to save playlists on bot's server
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "100"))
@@ -145,21 +145,21 @@ clean = {}
 autoclean = []
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
+    "START_IMG_URL", "https://graph.org/file/ec13ab3d0686426d8a863.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
+    "PING_IMG_URL", "https://graph.org/file/b04b1f19dd93a0669f410.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
+PLAYLIST_IMG_URL = "https://graph.org/file/9b2806ad85125926091e7.jpg"
+STATS_IMG_URL = "https://graph.org/file/9b2806ad85125926091e7.jpg"
+TELEGRAM_AUDIO_URL = "https://graph.org/file/9b2806ad85125926091e7.jpg"
+TELEGRAM_VIDEO_URL = "https://graph.org/file/9b2806ad85125926091e7.jpg"
+STREAM_IMG_URL = "https://graph.org/file/9b2806ad85125926091e7.jpg"
+SOUNCLOUD_IMG_URL = "https://graph.org/file/9b2806ad85125926091e7.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/9b2806ad85125926091e7.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/9b2806ad85125926091e7.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/9b2806ad85125926091e7.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/9b2806ad85125926091e7.jpg"
 
 
 def time_to_seconds(time):
